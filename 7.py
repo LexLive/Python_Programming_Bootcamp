240f9eeb9d480b00f74a12d11991e97bcdae2161
LexLive <76833027+LexLive@users.noreply.github.com>
2021-02-01 22:53:23 +0700

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,28 @@
+import sys
+
+
+my_age = 37
+my_name = "lek"
+
+print("Hello", my_name, my_age)
+
+str_1 = "\"This is a quote\"" #needed to double quote inside string known as escape sequance
+print(str_1)
+# Comment
+
+'''
+Newline : \n
+Backslash : \\
+Single quote : \'
+Backspace : \b
+Tab : \t
+'''
+
+print(sys.maxsize)
+print(sys.float_info.max)
+
+f1 = 1.1111111111111111
+f2 = 1.1111111111111111
+f3 = f1 + f2
+print(f3)
+
